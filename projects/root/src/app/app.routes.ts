3bc68edc6e33c72634c42491bdfbbdfc789d4063
Aka <jenny50626000@gmail.com>
2024-03-14 10:34:39 +0800

    [feat] Import Module layout
    Changes to be committed:
    	modified:   projects/root/src/app/app.routes.ts
    	new file:   projects/root/src/app/decl.d.ts
    	modified:   projects/root/webpack.config.js

--- projects/root/src/app/app.routes.ts
+++ projects/root/src/app/app.routes.ts
@@ -1,4 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [{ path: '', component: MainComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'header',
+    loadChildren: () => import('layout/Module').then((m) => m.LayoutModule),
+  },
+];
